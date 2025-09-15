c4d9987c8191f7444fd71ce1555ddb9582ac1294
Ender17k <alejandro.17est@gmail.com>
2025-09-14 18:58:44 -0600

    Add files via upload

---
+++ ejercicio1.js
@@ -0,0 +1,10 @@
+const tableroPuntuacion = {
+  Percy: 300,
+  Annabeth: 280,
+  Grover: 270
+};
+
+console.log(tableroPuntuacion);
+
+// Resultado esperado:
+// { Percy: 300, Annabeth: 280, Grover: 270 }
